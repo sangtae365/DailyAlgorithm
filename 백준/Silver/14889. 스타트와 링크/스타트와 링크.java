6de54f41e77a0867d20d238e2ef3e4c9667a8163
sangtae365 <95201136+sangtae365@users.noreply.github.com>
2022-10-27 23:20:38 +0900

    [Silver II] Title: 스타트와 링크, Time: 328 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,map[][],ans;
+	static boolean visited[];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		ans = Integer.MAX_VALUE;
+		visited = new boolean[N];
+		comb(0,0);
+		System.out.println(ans);
+	}
+	public static void comb(int cnt, int start) {
+		if(cnt == N/2) {
+			diff();
+			return;
+		}
+		
+		for(int i=start;i<N;i++) {
+			visited[i] = true;
+			comb(cnt+1, i+1);
+			visited[i] = false;
+		}
+	}
+	public static void diff() {
+		int start = 0;
+		int link = 0;
+		
+		for(int i=0;i<N-1;i++) {
+			for(int j=i+1;j<N;j++) {
+				if(visited[i] && visited[j]) {
+					start += (map[i][j] + map[j][i]);
+				}else if(!visited[i] && !visited[j]) {
+					link += (map[i][j] + map[j][i]);
+				}
+			}
+		}
+		
+		ans = Math.min(ans, Math.abs(start -link));
+	}
+}
