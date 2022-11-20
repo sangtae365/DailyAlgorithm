2e046663112b0622643a9b5fcd29ccea65e4b787
박상태 <95201136+sangtae365@users.noreply.github.com>
2022-11-20 10:36:29 +0900

    [Silver V] Title: 단어 정렬, Time: 264 ms, Memory: 28068 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		String arr[] = new String[N];
+		for(int i=0;i<N;i++) {
+			arr[i] = br.readLine();
+		}
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.length()==o2.length()) {
+					return o1.compareTo(o2);
+				}else {
+					return o1.length()-o2.length();
+				}
+			}
+		});
+		sb.append(arr[0]).append("\n");
+		for(int i=1;i<N;i++) {
+			if(arr[i-1].equals(arr[i]))continue;
+			sb.append(arr[i]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
