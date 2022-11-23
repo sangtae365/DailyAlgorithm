84005d6b46b0d4aabccfb4ac73b2d56c5fba942c
박상태 <95201136+sangtae365@users.noreply.github.com>
2022-11-23 10:16:18 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.58 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        int N = A.length;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i=0;i<N;i++){
+            answer+=A[i]*B[N-i-1];
+        }
+        
+        return answer;
+    }
+}
