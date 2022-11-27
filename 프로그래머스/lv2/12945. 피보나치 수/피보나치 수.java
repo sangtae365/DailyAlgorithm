18703317aa0b436bc751a491c65f4752c4db0c84
박상태 <95201136+sangtae365@users.noreply.github.com>
2022-11-27 22:49:14 +0900

    [level 2] Title: 피보나치 수, Time: 3.48 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int fibo[] = new int[n];
+        
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for(int i=2;i<n;i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+        }
+        answer = (fibo[n-1] + fibo[n-2])%1234567;
+        
+        return answer;
+    }
+}
