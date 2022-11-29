ed3370152459463dde85abf3b4519c7064c69d1a
박상태 <95201136+sangtae365@users.noreply.github.com>
2022-11-29 21:23:37 +0900

    [Silver V] Title: 뒤집기, Time: 92 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		int cnt = 0;
+		boolean flag = true;
+		if(s.charAt(0) == '0') {
+			for(int i=1;i<s.length();i++) {
+				if(flag && s.charAt(i) == '1') {
+					flag = false;
+					cnt++;
+				}
+				if(!flag && s.charAt(i) == '0')flag = true;
+			}
+		}else {
+			for(int i=1;i<s.length();i++) {
+				if(flag && s.charAt(i) == '0') {
+					flag = false;
+					cnt++;
+				}
+				if(!flag && s.charAt(i) == '1')flag = true;
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
