ce8e9df4901ad79f0067fdb26f60c384fe67dc23
박상태 <95201136+sangtae365@users.noreply.github.com>
2022-11-29 16:15:04 +0900

    [level 2] Title: 구명보트, Time: 25.95 ms, Memory: 56 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int i = 0;
+        Arrays.sort(people);
+        
+        for(int j=people.length-1;j>=i;j--){
+            if(people[i]+people[j]<=limit){
+                answer++;
+                i++;
+            }else{
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
