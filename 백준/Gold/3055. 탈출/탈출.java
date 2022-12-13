e788832434b1d1853ac232d6f46591d9f3af4299
박상태 <95201136+sangtae365@users.noreply.github.com>
2022-12-14 00:32:22 +0900

    [Gold IV] Title: 탈출, Time: 80 ms, Memory: 11816 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -6,71 +6,77 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int R, C, ans;
-	static char map[][];
-	static int dx[] = {0,1,0,-1};
-	static int dy[] = {1,0,-1,0};
-	static Queue<int []> waterQ, gosumQ;
+	static Queue<int []> water_q, gosum_q;
+	static int dx[] = {1,0,-1,0};
+	static int dy[] = {0,1,0,-1};
+	static int R,C,ans;
+	static char[][] map;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		water_q = new LinkedList<>();
+		gosum_q = new LinkedList<>();
+		
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		
-		waterQ = new LinkedList<>();
-		gosumQ = new LinkedList<>();
-		
 		map = new char[R][C];
+		
 		for(int i=0;i<R;i++) {
 			String s = br.readLine();
 			for(int j=0;j<C;j++) {
 				map[i][j] = s.charAt(j);
-				if(map[i][j] == 'S')gosumQ.add(new int[] {i,j,0});
-				else if(map[i][j] == '*')waterQ.add(new int[] {i,j});
+				if(map[i][j] == 'S') {
+					gosum_q.add(new int[] {i,j,0});
+				}else if(map[i][j] == '*') {
+					water_q.add(new int[] {i,j});
+				}
 			}
 		}
 		ans = Integer.MAX_VALUE;
+		
 		bfs();
+		
 		System.out.println(ans == Integer.MAX_VALUE ? "KAKTUS" : ans);
 	}
-	public static void bfs() {
-		while(!gosumQ.isEmpty()) {
-			int len = waterQ.size();
-			for(int t=0;t<len;t++) {
-				int temp[] = waterQ.poll();
+
+	private static void bfs() {
+		while(!gosum_q.isEmpty()) {
+			int len = water_q.size();
+			for(int i=0;i<len;i++) {
+				int temp[] = water_q.poll();
 				int x = temp[0];
 				int y = temp[1];
-				for(int i=0;i<4;i++) {
-					int nx = x+dx[i];
-					int ny = y+dy[i];
+				
+				for(int j=0;j<4;j++) {
+					int nx = x+dx[j];
+					int ny = y+dy[j];
 					
-					if(nx>=0 && nx<R && ny>=0 && ny<C && map[nx][ny] == '.') {
+					if(nx<0 || nx>=R || ny<0 || ny>=C)continue;
+					if(map[nx][ny] == '.') {
 						map[nx][ny] = '*';
-						waterQ.add(new int[] {nx,ny});
+						water_q.add(new int[] {nx,ny});
 					}
 				}
 			}
-			
-			len = gosumQ.size();
-			for(int t=0;t<len;t++) {
-				int g[] = gosumQ.poll();
-				int x = g[0];
-				int y = g[1];
-				int w = g[2];
+			len = gosum_q.size();
+			for(int i=0;i<len;i++) {
+				int temp[] = gosum_q.poll();
+				int x = temp[0];
+				int y = temp[1];
+				int time = temp[2];
 				
-				for(int i=0;i<4;i++) {
-					int nx = x+dx[i];
-					int ny = y+dy[i];
+				for(int j=0;j<4;j++) {
+					int nx = x+dx[j];
+					int ny = y+dy[j];
 					
-					if(nx>=0 && nx<R && ny>=0 && ny<C) {
-						if(map[nx][ny] == 'D') {
-							ans = Math.min(ans, w+1);
-							return;
-						}else if(map[nx][ny] == '.') {
-							map[nx][ny] = 'S';
-							gosumQ.add(new int[] {nx,ny,w+1});
-						}
+					if(nx<0 || nx>=R || ny<0 || ny>=C)continue;
+					if(map[nx][ny] == '.') {
+						gosum_q.add(new int[] {nx,ny,time+1});
+						map[nx][ny] = 'S';
+					}else if(map[nx][ny] == 'D') {
+						ans = Math.min(ans, time+1);
+						return;
 					}
 				}
 			}
